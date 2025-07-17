b9c09ecb26fea155081f528fafd936607654e47b
Elijah Luis <82965468+ElijahLuis@users.noreply.github.com>
2025-07-16 22:01:36 -0500

    Load camera patch lazily

--- src/PlayerModule/CameraModule/Patch.lua
+++ src/PlayerModule/CameraModule/Patch.lua
@@ -1,9 +1,8 @@
 --!strict
-local PatchCameraModule = require(game.ReplicatedStorage.Wallstick.PatchCameraModule)
-
 local Patch = {}
 
 function Patch.redirect(cameraModule: any)
+	local PatchCameraModule = require(game.ReplicatedStorage.Wallstick.PatchCameraModule)
 	PatchCameraModule(cameraModule)
 end
 
