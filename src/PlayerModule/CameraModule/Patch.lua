7b36e35a1897ccdd656e566bf317c3c0277438a2
Elijah Luis <82965468+ElijahLuis@users.noreply.github.com>
2025-07-16 21:39:08 -0500

    Fix camera patch wiring

--- src/PlayerModule/CameraModule/Patch.lua
+++ src/PlayerModule/CameraModule/Patch.lua
@@ -1,4 +1,10 @@
 --!strict
+local PatchCameraModule = require(game.ReplicatedStorage.Wallstick.PatchCameraModule)
+
 local Patch = {}
+
+function Patch.redirect(cameraModule: any)
+	PatchCameraModule(cameraModule)
+end
 
 return Patch
