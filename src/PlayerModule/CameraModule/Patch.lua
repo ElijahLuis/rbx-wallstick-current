3351188d86a60254a4f10dfc2d0f4f9b66fa689a
Elijah Luis <82965468+ElijahLuis@users.noreply.github.com>
2025-07-16 22:44:14 -0500

    Guard camera patch and load from PlayerModule

--- src/PlayerModule/CameraModule/Patch.lua
+++ src/PlayerModule/CameraModule/Patch.lua
@@ -4,7 +4,7 @@ local patched = setmetatable({}, { __mode = "k" })
 local Patch = {}
 
 function Patch.redirect(cameraModule: any)
-	if not patched[cameraModule] then
+	if typeof(cameraModule) == "table" and not patched[cameraModule] then
 		patched[cameraModule] = true
 		local patchCamera = require(game.ReplicatedStorage.Wallstick.PatchCameraModule)
 		patchCamera(cameraModule)
